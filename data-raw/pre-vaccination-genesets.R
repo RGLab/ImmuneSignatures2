60f9585f50bb2271ab4459a52fe4e8bfe80c325e
Helen Miller <hmiller@fredhutch.org>
2021-01-11 15:19:09 -0800

    Update anno on genesets

--- data-raw/pre-vaccination-genesets.R
+++ data-raw/pre-vaccination-genesets.R
@@ -1,7 +1,10 @@
 # Pre-vaccination Manuscript data generation
 library(tidyverse)
+library(UpdateAnno)
+library(data.table)
+library(here)
 
-gmxFile <- "data-raw/msigdb.v7.1.symbols.gmt"
+gmxFile <- here("data-raw", "msigdb.v7.1.symbols.gmt")
 # read hallmark
 cNames <- count_fields(file = gmxFile, tokenizer = tokenizer_tsv()) %>%
   max() %>%
@@ -16,7 +19,7 @@ hallmark_geneset <- read_tsv(file = gmxFile, col_names = cNames) %>%
   unstack()
 
 # load BTM (from qusage)
-load(file = "data-raw/GeneSets.rda")
+load(file = here("data-raw", "GeneSets.rda"))
 btmLS <- stack(BTM.geneSets) %>%
   as.data.frame() %>%
   rowid_to_column()
@@ -31,4 +34,31 @@ btmLS$values %>%
 
 # append to list of genesets
 all_genesets <- c(hallmark_geneset, btmLS)
-usethis::use_data(all_genesets)
+
+# Update to latest anno
+if (packageVersion("UpdateAnno") != "3.9.0") stop("Install UpdateAnno v3.9.0!")
+data("hgncAlias2Symbol")
+# Copied from updateDataWithLatestHGNCMap vignette from UpdateAnno
+updateModuleSymbols <- function(module, hgncAlias2Symbol){
+
+  # read in as DT
+  modList <- get(module)
+  modDT <- rbindlist(lapply(modList, data.table), idcol = "module")
+  setnames(modDT, "V1", "ALIAS")
+
+  # update gene symbols
+  modDT[hgncAlias2Symbol, SYMBOL := SYMBOL, on = c(ALIAS = "ALIAS")]
+  setnames(modDT, "module", "pathway")
+  modDT[, ALIAS := NULL]
+  modDT <- modDT[ !is.na(modDT$SYMBOL) ]
+  modDT <- unique(modDT) # MUST DE-DUPE so that gmt file can be created, checked by GeneSet()
+
+  # save with correct name
+  upMod <- plyr::dlply(modDT, 1, function(x){ x$SYMBOL }) # convert to list of lists
+
+  return(upMod)
+}
+all_genesets <- updateModuleSymbols("all_genesets", hgncAlias2Symbol)
+btm_list <- updateModuleSymbols("btmLS", hgncAlias2Symbol)
+usethis::use_data(all_genesets, overwrite = TRUE)
+usethis::use_data(btm_list, overwrite = TRUE)
