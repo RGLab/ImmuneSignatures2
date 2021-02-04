08053b84ec649b084267ca367726e34b0e2b3792
Helen Miller <hmiller@fredhutch.org>
2021-02-04 14:42:23 -0800

    Add pca plots vignette

--- data-raw/build_esets.R
+++ data-raw/build_esets.R
@@ -1,21 +1,28 @@
-outputDir = here::here("outputs", "virtualStudy_prod_2021_1_19")
-dataCacheDir = here::here("data_cache", "virtualStudy_prod_2021_1_19")
-timestamp = "2021_1_19_"
+outputDir = here::here("outputs", "2020_12_23_newAnno")
+dataCacheDir = here::here("data_cache")
+timestamp = "2020_12_23_newAnno_"
 
 if (!dir.exists(outputDir)) dir.create(outputDir, recursive = TRUE)
 if (!dir.exists(dataCacheDir)) dir.create(dataCacheDir, recursive = TRUE)
 
-rmarkdown::render(input = here::here("data-raw", "generate_base_eset.Rmd"),
-       output_file = here::here(outputDir, "generate_base_eset.html"),
-       params = list(
-         outputDir = outputDir,
-         dataCacheDir = dataCacheDir,
-         timestamp = timestamp
-       ))
-rmarkdown::render(input = here::here("data-raw", "create_final_esets.Rmd"),
-       output_file = here::here(outputDir, "create_final_esets.html"),
-       params = list(
-         outputDir = outputDir,
-         dataCacheDir = dataCacheDir,
-         timestamp = timestamp
-       ))
+# rmarkdown::render(input = here::here("data-raw", "generate_base_eset.Rmd"),
+#        output_file = here::here(outputDir, "generate_base_eset.html"),
+#        params = list(
+#          outputDir = outputDir,
+#          dataCacheDir = dataCacheDir,
+#          timestamp = timestamp
+#        ))
+# rmarkdown::render(input = here::here("data-raw", "create_final_esets.Rmd"),
+#        output_file = here::here(outputDir, "create_final_esets.html"),
+#        params = list(
+#          outputDir = outputDir,
+#          dataCacheDir = dataCacheDir,
+#          timestamp = timestamp
+#        ))
+rmarkdown::render(input = here::here("vignettes", "pca_plots.Rmd"),
+                  output_file = here::here(outputDir, "pca_plots.html"),
+                  params = list(
+                          outputDir = file.path(outputDir, "pca"),
+                          dataCacheDir = dataCacheDir,
+                          timestamp = timestamp
+                  ))
