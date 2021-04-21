ace5c5c69a60f11e794968aec5f793d6745d6d01
Helen Miller <hmiller@fredhutch.org>
2021-04-21 09:38:40 -0700

    Update dependencies

--- data-raw/build_esets.R
+++ data-raw/build_esets.R
@@ -1,24 +1,24 @@
-outputDir = here::here("outputs", "2021_03_08")
+outputDir = here::here("outputs", "tmp")
 dataCacheDir = here::here("data_cache", "2021_03_08")
 timestamp = "2021_03_08_"
 
 if (!dir.exists(outputDir)) dir.create(outputDir, recursive = TRUE)
 if (!dir.exists(dataCacheDir)) dir.create(dataCacheDir, recursive = TRUE)
 
-# rmarkdown::render(input = here::here("data-raw", "generate_base_eset.Rmd"),
-#        output_file = here::here(outputDir, "generate_base_eset.html"),
-#        params = list(
-#          outputDir = outputDir,
-#          dataCacheDir = dataCacheDir,
-#          timestamp = timestamp
-#        ))
-# rmarkdown::render(input = here::here("data-raw", "create_final_esets.Rmd"),
-#        output_file = here::here(outputDir, "create_final_esets.html"),
-#        params = list(
-#          outputDir = outputDir,
-#          dataCacheDir = dataCacheDir,
-#          timestamp = timestamp
-#        ))
+rmarkdown::render(input = here::here("data-raw", "generate_base_eset.Rmd"),
+       output_file = here::here(outputDir, "generate_base_eset.html"),
+       params = list(
+         outputDir = outputDir,
+         dataCacheDir = dataCacheDir,
+         timestamp = timestamp
+       ))
+rmarkdown::render(input = here::here("data-raw", "create_final_esets.Rmd"),
+       output_file = here::here(outputDir, "create_final_esets.html"),
+       params = list(
+         outputDir = outputDir,
+         dataCacheDir = dataCacheDir,
+         timestamp = timestamp
+       ))
 # rmarkdown::render(input = here::here("vignettes", "pca_plots.Rmd"),
 #                   output_file = here::here(outputDir, "pca_plots.html"),
 #                   params = list(
@@ -26,7 +26,7 @@ if (!dir.exists(dataCacheDir)) dir.create(dataCacheDir, recursive = TRUE)
 #                           dataCacheDir = dataCacheDir,
 #                           timestamp = timestamp
 #                   ))
-#
+
 # rmarkdown::render(input = here::here("vignettes", "original_code", "IOF_RAPToR_age_imputation_all_functions_021121.Rmd"),
 #                   output_file = here::here(outputDir, "RAPToR_all_functions.html"),
 #                   output_format = "html_document",
@@ -34,11 +34,11 @@ if (!dir.exists(dataCacheDir)) dir.create(dataCacheDir, recursive = TRUE)
 #                     dataCacheDir = dataCacheDir,
 #                     timestamp = timestamp
 #                   ))
-
-rmarkdown::render(input = here::here("vignettes", "original_code", "IOF_RAPToR_age_imputation_best_functions_021121.Rmd"),
-                  output_file = here::here(outputDir, "RAPToR_best_functions.html"),
-                  output_format = "html_document",
-                  params = list(
-                    dataCacheDir = dataCacheDir,
-                    timestamp = timestamp
-                  ))
+#
+# rmarkdown::render(input = here::here("vignettes", "original_code", "IOF_RAPToR_age_imputation_best_functions_021121.Rmd"),
+#                   output_file = here::here(outputDir, "RAPToR_best_functions.html"),
+#                   output_format = "html_document",
+#                   params = list(
+#                     dataCacheDir = dataCacheDir,
+#                     timestamp = timestamp
+#                   ))
