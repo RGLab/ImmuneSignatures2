5b600071779750c419066a73249079a14e97f1aa
J. Kim <jkim2345@fredhutch.org>
2022-09-28 11:19:19 -0700

    Rename all "Live attenuated" to "Live virus" (Slim/Thomas)

--- data-raw/vaccines.R
+++ data-raw/vaccines.R
@@ -1,3 +1,3 @@
 vaccines <- data.table::fread("data-raw/vaccine_map.csv", header = TRUE)
-vaccines <- vaccines[, V1 := NULL ]
+vaccines <- vaccines[, V1 := NULL]
 usethis::use_data(vaccines, overwrite = TRUE)
